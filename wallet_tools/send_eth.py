b6064f758d6fa77b68e0f286140bfab902e3c702
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-14 11:27:41 +0900

    Broadcasting the transaction to the network

--- wallet_tools/send_eth.py
+++ wallet_tools/send_eth.py
@@ -15,3 +15,8 @@ def send_eth(to_address: str, amount_eth: float) -> str:
         "gasPrice": w3.to_wei("20", "gwei"),
         "chainId": 11155111,
     }
+
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+    return w3.to_hex(tx_hash)
