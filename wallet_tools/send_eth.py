e5d6b835b7bd307336f2772af8beecff4cb93d7e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-10-19 13:16:16 +0900

    Debugging to fix the sending transaction error

--- wallet_tools/send_eth.py
+++ wallet_tools/send_eth.py
@@ -21,6 +21,6 @@ def send_eth(to_address: str, amount_eth: float) -> str:
     }
 
     signed_tx = w3.eth.account.sign_transaction(tx, private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
     return w3.to_hex(tx_hash)
